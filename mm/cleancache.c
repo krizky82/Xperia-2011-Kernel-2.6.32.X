599810f59bcb23c19cfd92c0b3b71dac2ecdc8ed
Kiki Rizky <kiki.rizky82@gmail.com>
2012-04-14 15:47:00 +0700

    mm: cleancache: Use __read_mostly as appropiate.
    From: Dan Magenheimer <dan.magenheimer@oracle.com>
    
    The values are rarely changed so might as well put them in the
    appropiate section.
    
    Signed-off-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Reviewed-by: Konrad Wilk <konrad.wilk@oracle.com>
    Cc: Kamezawa Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Jan Beulich <JBeulich@novell.com>
    Cc: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Cc: Jeremy Fitzhardinge <jeremy@goop.org>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Matthew Wilcox <matthew@wil.cx>
    Cc: Chris Mason <chris.mason@oracle.com>
    Cc: Rik Riel <riel@redhat.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>

--- mm/cleancache.c
+++ mm/cleancache.c
@@ -24,14 +24,14 @@
  * disabled), so is preferred to the slower alternative: a function
  * call that checks a non-global.
  */
-int cleancache_enabled;
+int cleancache_enabled __read_mostly;
 EXPORT_SYMBOL(cleancache_enabled);
 
 /*
  * cleancache_ops is set by cleancache_ops_register to contain the pointers
  * to the cleancache "backend" implementation functions.
  */
-static struct cleancache_ops cleancache_ops;
+static struct cleancache_ops cleancache_ops __read_mostly;
 
 /* useful stats available in /sys/kernel/mm/cleancache */
 static unsigned long cleancache_succ_gets;
