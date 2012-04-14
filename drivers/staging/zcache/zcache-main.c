9102e6f5977b5179e1238ecfd28219b6c96604c8
Kiki Rizky <kiki.rizky82@gmail.com>
2012-04-14 11:51:00 +0700

    [PATCH] zcache: Set SWIZ_BITS to 8 to reduce tmem bucket lock  contention.
    SWIZ_BITS > 8 results in a much larger number of "tmem_obj"
    allocations, likely one per page-placed-in-frontswap.  The
    tmem_obj is not huge (roughly 100 bytes), but it is large
    enough to add a not-insignificant memory overhead to zcache.
    
    The SWIZ_BITS=8  will get roughly the same lock contention
    without the space wastage.
    
    The effect of SWIZ_BITS can be thought of as "2^SWIZ_BITS is
    the number of unique oids that be generated" (This concept is
    limited to frontswap's use of tmem).
    
    Acked-by: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -1354,6 +1354,7 @@ static void zcache_cleancache_put_page(int pool_id,
 
 	if (!PageWasActive(page))
 		return;
+
 	if (likely(ind == index))
 		(void)zcache_put_page(pool_id, &oid, index, page);
 }
@@ -1368,8 +1369,10 @@ static int zcache_cleancache_get_page(int pool_id,
 
 	if (likely(ind == index))
 		ret = zcache_get_page(pool_id, &oid, index, page);
+
 	if (ret == 0)
 		SetPageWasActive(page);
+
 	return ret;
 }
 
@@ -1442,9 +1445,9 @@ static int zcache_frontswap_poolid = -1;
  * Swizzling increases objects per swaptype, increasing tmem concurrency
  * for heavy swaploads.  Later, larger nr_cpus -> larger SWIZ_BITS
  * Setting SWIZ_BITS to 27 basically reconstructs the swap entry from
- * frontswap_get_page()
+ * frontswap_get_page(), but has side-effects. Hence using 8
  */
-#define SWIZ_BITS		27
+#define SWIZ_BITS		8
 #define SWIZ_MASK		((1 << SWIZ_BITS) - 1)
 #define _oswiz(_type, _ind)	((_type << SWIZ_BITS) | (_ind & SWIZ_MASK))
 #define iswiz(_ind)		(_ind >> SWIZ_BITS)
